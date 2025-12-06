4332cd325aada094f514e462cd57d7e1e3e46424
Suresh Thapa <sureshthapa6063@gmail.com>
2025-12-06 19:18:10 +0545

    refactor cart services for improved readability and consistency

--- client/src/api/cartServices.js
+++ client/src/api/cartServices.js
@@ -5,18 +5,62 @@ import { useCartStore } from "../zustand/useCartStore";
 
 // get user cart
 export const useCarts = () => {
-  const {setUserCart} = useCartStore()
+  const { setUserCart } = useCartStore();
 
   return useQuery({
     queryKey: ["carts"],
     queryFn: async () => {
       const response = await apiRequest.get("/carts/");
-      setUserCart(response.data) // setting user cart info to zustand cart state
+      setUserCart(response.data); // setting user cart info to zustand cart state
       return response.data;
     },
   });
-}
-  
+};
+
+// add to cart
+export const useAddToCart = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async ({ product, quantity }) =>
+      await apiRequest.post("/carts/add/", {
+        product,
+        quantity,
+      }),
+    onSuccess: (data) => {
+      // invalidate fetch cart key
+
+      queryClient.invalidateQueries(["carts"]);
+      toast.success("item added to cart sucessfully.");
+      console.log(data);
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+};
+
+
+// update to cart
+export const useUpdateToCart = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async ({cart, quantity }) =>
+      await apiRequest.patch(`/carts/update/${cart}/`, {
+        quantity,
+      }),
+    onSuccess: () => {
+      // invalidate fetch cart key
+
+      queryClient.invalidateQueries(["carts"]);
+      toast.success("Cart item updated sucessfully.");
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+};
 
 // delete from cart
 export const useRemoveFromCart = () => {
