d383b176f5d094df3a3e9c87e0dd9b1af8d91d86
Suresh Thapa <sureshthapa6063@gmail.com>
2025-12-07 18:43:36 +0545

    refactor SearchBar component to handle search input and submission

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,11 +1,32 @@
-import { Search } from "lucide-react"
+import { Search } from "lucide-react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const SearchBar = () => {
+  const navigate = useNavigate();
+  const [serach, setSearch] = useState("");
+
+  const handleSearch = (e) => {
+    e.preventDefault()
+    navigate(`/products/?search=${serach}`);
+  };
+
   return (
-    <div className="md:flex gap-2 items-center border border-gray-300 py-2 px-4 rounded-full hidden">
-        <Search className="text-gray-400 cursor-pointer hover:text-gray-600 transition" size={20} />
-        <input type="text" placeholder="search..." className="focus:outline-none" />
-    </div>
-  )
-}
-export default SearchBar
+    <form onSubmit={handleSearch}>
+      <div className="md:flex gap-2 items-center border border-gray-300 py-2 px-4 rounded-full hidden">
+        <Search
+          onClick={handleSearch}
+          className="text-gray-400 cursor-pointer hover:text-gray-600 transition"
+          size={20}
+        />
+        <input
+          onChange={(e) => setSearch(e.target.value)}
+          type="text"
+          placeholder="search..."
+          className="focus:outline-none"
+        />
+      </div>
+    </form>
+  );
+};
+export default SearchBar;
